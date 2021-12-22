a6231c2f39c782c7068a9616e458cd041b6cb634
Evan Godon <e.godon@outlook.com>
2021-12-21 21:22:39 -0500

    split out list command for TILs and journals

--- cmd/list.go
+++ cmd/list.go
@@ -1,126 +1,15 @@
 package cmd
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
-	"jrnl/pkg/sqldb"
-	"jrnl/pkg/ui"
-	"jrnl/pkg/util"
-	"log"
-	"time"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/glamour"
-	"github.com/uptrace/bun"
 	"github.com/urfave/cli/v2"
 )
 
 var ListCmd = &cli.Command{
 	Name:    "list",
 	Aliases: []string{"l"},
-	Usage:   "Create a new journal entry for today",
-	Action: func(c *cli.Context) error {
-
-		p := tea.NewProgram(initialModel(c))
-		p.EnterAltScreen()
-
-		if err := p.Start(); err != nil {
-			fmt.Printf("Error occured: %v", err)
-			return err
-		}
-
-		return nil
+	Usage:   "List entries",
+	Subcommands: []*cli.Command{
+		ListJournalsCmd,
+		ListTilsCmd,
 	},
-	Flags: []cli.Flag{
-		&cli.BoolFlag{Name: "week", Aliases: []string{"w"}, Usage: "Show the week's entries"},
-	},
-}
-
-type model struct {
-	list ui.List
-	ctx  *cli.Context
-}
-
-type item struct {
-	itemNum   int
-	CreatedAt time.Time
-	Content   string
-}
-
-func (i item) Title() string { return fmt.Sprintf("Journal #%d", i.itemNum) }
-
-func (i item) Description() string {
-	return util.FormatToLocalTime(i.CreatedAt, "Monday, January 2, 2006")
-}
-
-func (i item) GetContent() string {
-	out, err := glamour.Render(i.Content, "dark")
-	util.CheckError(err)
-
-	return out
-}
-
-func (i item) FilterValue() string { return i.CreatedAt.String() }
-
-func initialModel(c *cli.Context) model {
-	return model{
-		list: ui.CreateList("Journal Entries"),
-		ctx:  c,
-	}
-}
-
-func getJournalEntries(c *cli.Context) tea.Msg {
-	var (
-		db  *bun.DB         = sqldb.Connect()
-		ctx context.Context = context.Background()
-	)
-
-	whereClause := "true"
-	if c.Bool("week") {
-		whereClause = "created_at >= date('now', 'weekday 0', '-7 days')"
-	}
-
-	var journalEntries []item
-
-	err := db.NewSelect().
-		Model(&sqldb.Entry{}).
-		Column("created_at", "content").
-		Order("created_at DESC").
-		Where("type = ?", sqldb.EntryType.Journal).
-		Where(whereClause).
-		Scan(ctx, &journalEntries)
-
-	if err != nil {
-		if err != sql.ErrNoRows {
-			log.Fatal(err)
-		}
-	}
-
-	var items []ui.ListItem
-	for index, entry := range journalEntries {
-		var item ui.ListItem = item{itemNum: len(journalEntries) - index, CreatedAt: entry.CreatedAt, Content: entry.Content}
-		items = append(items, item)
-	}
-
-	return items
-}
-
-func (m model) Init() tea.Cmd {
-	teaCmd := func() tea.Msg {
-		return getJournalEntries(m.ctx)
-	}
-
-	return teaCmd
-
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	cmd := m.list.HandleMessage(msg)
-
-	return m, cmd
-}
-
-func (m model) View() string {
-	return m.list.View()
 }
---
+++ cmd/listTILs.go
@@ -0,0 +1,126 @@
+package cmd
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"jrnl/pkg/sqldb"
+	"jrnl/pkg/ui"
+	"jrnl/pkg/util"
+	"log"
+	"strings"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/uptrace/bun"
+	"github.com/urfave/cli/v2"
+)
+
+var ListTilsCmd = &cli.Command{
+	Name:    "til",
+	Aliases: []string{"t"},
+	Usage:   "List all TILs entries",
+	Action: func(c *cli.Context) error {
+
+		p := tea.NewProgram(initialListTilsModel(c))
+		p.EnterAltScreen()
+
+		if err := p.Start(); err != nil {
+			fmt.Printf("Error occured: %v", err)
+			return err
+		}
+
+		return nil
+	},
+}
+
+type listTilsModel struct {
+	list ui.List
+	ctx  *cli.Context
+}
+
+type tilItem struct {
+	itemNum   int
+	CreatedAt time.Time
+	Content   string
+}
+
+func (i tilItem) Title() string { return fmt.Sprintf("TIL #%d", i.itemNum) }
+
+func (i tilItem) Description() string {
+	maxLength := 50
+	description := i.Content
+
+	if len(i.Content) > maxLength {
+		description = strings.Split(i.Content, "")[0][:maxLength]
+	}
+
+	return strings.TrimSpace(description)
+
+}
+
+func (i tilItem) GetContent() string {
+	out, err := glamour.Render(i.Content, "dark")
+	util.CheckError(err)
+
+	return out
+}
+
+func (i tilItem) FilterValue() string { return i.CreatedAt.String() }
+
+func initialListTilsModel(c *cli.Context) listTilsModel {
+	return listTilsModel{
+		list: ui.CreateList("Journal Entries"),
+		ctx:  c,
+	}
+}
+
+func getTilsEntries(c *cli.Context) tea.Msg {
+	var (
+		db  *bun.DB         = sqldb.Connect()
+		ctx context.Context = context.Background()
+	)
+
+	var tilEntries []tilItem
+
+	err := db.NewSelect().
+		Model(&sqldb.Entry{}).
+		Column("created_at", "content").
+		Order("created_at DESC").
+		Where("type = ?", sqldb.EntryType.TIL).
+		Scan(ctx, &tilEntries)
+
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Fatal(err)
+		}
+	}
+
+	var items []ui.ListItem
+	for index, entry := range tilEntries {
+		var item ui.ListItem = tilItem{itemNum: len(tilEntries) - index, CreatedAt: entry.CreatedAt, Content: entry.Content}
+		items = append(items, item)
+	}
+
+	return items
+}
+
+func (m listTilsModel) Init() tea.Cmd {
+	teaCmd := func() tea.Msg {
+		return getTilsEntries(m.ctx)
+	}
+
+	return teaCmd
+
+}
+
+func (m listTilsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	cmd := m.list.HandleMessage(msg)
+
+	return m, cmd
+}
+
+func (m listTilsModel) View() string {
+	return m.list.View()
+}
