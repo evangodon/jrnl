777f310cbf15f4e6c14512281c3f11bdb903a8c2
Evan Godon <e.godon@outlook.com>
2022-11-01 10:22:59 -0400

    fix: date format in web app

--- app/src/routes/+page.ts
+++ app/src/routes/+page.ts
@@ -3,32 +3,32 @@ import dayjs from 'dayjs';
 import type { PageLoad } from './$types';
 
 type Response = {
-	daily: {
-		id: string;
-		content: string;
-		createdAt: Date;
-		updatedAt: Date;
-	};
+  daily: {
+    id: string;
+    content: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
 };
 
 export const load: PageLoad = async ({ fetch, depends }) => {
-	depends('daily-entry:new');
-	const date = dayjs().format('YYYY-MM-D');
-	try {
-		const response = await api.get(`daily/${date}`, { fetch });
+  depends('daily-entry:new');
+  const date = dayjs().format('YYYY-MM-DD');
+  try {
+    const response = await api.get(`daily/${date}`, { fetch });
 
-		return await response.json<Response>();
-	} catch (err: unknown) {
-		// TODO: if error is 404
-		const response = await api.get(`daily/template`, { fetch });
-		const data = await response.json<{ template: string }>();
-		return {
-			daily: {
-				id: '-1',
-				content: data.template
-			}
-		};
-	}
+    return await response.json<Response>();
+  } catch (err: unknown) {
+    // TODO: if error is 404
+    const response = await api.get(`daily/template`, { fetch });
+    const data = await response.json<{ template: string }>();
+    return {
+      daily: {
+        id: '-1',
+        content: data.template
+      }
+    };
+  }
 };
 
 export const ssr = false;
